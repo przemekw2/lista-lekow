d5e1db9f9eb88521242c980496d0b275ce7a6240
Przemyslaw <przemekw2@gmail.com>
2019-01-11 11:08:49 +0000

    update datagrida, walidacja dawkowania, format dawkowania

---
+++ ListaLekow/Validation Rules Classes/DawkowanieValidator.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+using System.Globalization;
+
+namespace ListaLekow.Validation_Rules_Classes
+{
+    class DawkowanieValidator : ValidationRule
+    {
+
+        public override ValidationResult Validate(object value, System.Globalization.CultureInfo cultureInfo) 
+        {
+            if (value == null)
+                return new ValidationResult(false, "Wartość nie może być pusta.");
+            else
+            {
+                double dblValue;
+                string strValue = value.ToString();
+
+                if (Double.TryParse(strValue, System.Globalization.NumberStyles.AllowDecimalPoint, CultureInfo.InvariantCulture, out dblValue)) { }
+                else
+                {
+                    return new ValidationResult
+                    (false, "Wartość musi być liczbą.");
+                }
+                return ValidationResult.ValidResult;
+            }
+        }
+
+    }
+}
